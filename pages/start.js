f7a355264b2589597e63759f56753f35b91e17d7
aedit <udisen97@gmail.com>
2019-06-05 13:02:59 +0530

    Random Question Order

--- pages/start.js
+++ pages/start.js
@@ -4,6 +4,26 @@ import { connect } from 'react-redux'
 import quizzes from '../questions/quizzes'
 import NextHead from 'next/head'
 
+function shuffle(array) {
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex)
+    currentIndex -= 1
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex]
+    array[currentIndex] = array[randomIndex]
+    array[randomIndex] = temporaryValue
+  }
+
+  return array
+}
+
 const Start = props => {
   const name = props.name
   return (
@@ -11,7 +31,7 @@ const Start = props => {
       <NextHead>
         <title>{name} Quiz</title>
       </NextHead>
-      <QuizOffline questions={quizzes[name]} />
+      <QuizOffline questions={shuffle(quizzes[name])} />
     </>
   )
 }
